fefad117f70d28845d492cf8e6831b2898bfc6c3
lvshaoping007 <1244849816@qq.com>
2023-11-05 18:32:03 +0800

    add use client config

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
 
 
--- ui/src/pages/_app.page.tsx
+++ ui/src/pages/_app.page.tsx
@@ -1,8 +1,10 @@
+'use client'
+
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import './reactCOIServiceWorker';
-import StyledComponentsRegistry from '@/registry';
+import StyledComponentsRegistry from './registry';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <StyledComponentsRegistry><Component {...pageProps} /></StyledComponentsRegistry>
