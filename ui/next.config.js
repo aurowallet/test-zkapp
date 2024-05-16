0ab02440e9793d9b6f706caff4a1533bc1f8699c
lvshaoping007 <1244849816@qq.com>
2024-05-16 15:05:22 +0800

    Update: upgrade ui o1js to 1.2.0

--- ui/next.config.js
+++ ui/next.config.js
@@ -4,15 +4,15 @@ const nextConfig = {
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
 
 
-  webpack(config) {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      o1js: require('path').resolve('node_modules/o1js')
-    };
-    config.experiments = { ...config.experiments, topLevelAwait: true };
-    config.optimization.minimizer = [];
-    return config;
-  },
+  // webpack(config) {
+  //   config.resolve.alias = {
+  //     ...config.resolve.alias,
+  //     o1js: require('path').resolve('node_modules/o1js')
+  //   };
+  //   config.experiments = { ...config.experiments, topLevelAwait: true };
+  //   config.optimization.minimizer = [];
+  //   return config;
+  // },
   // To enable o1js for the web, we must set the COOP and COEP headers.
   // See here for more information: https://docs.minaprotocol.com/zkapps/how-to-write-a-zkapp-ui#enabling-coop-and-coep-headers
   async headers() {
