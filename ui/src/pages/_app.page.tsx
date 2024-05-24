dab142d097e3db574b81252203a84058ccb07ef4
lvshaoping007 <1244849816@qq.com>
2024-05-24 22:07:06 +0800

    Update headers

--- ui/src/pages/_app.page.tsx
+++ ui/src/pages/_app.page.tsx
@@ -3,7 +3,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-import './reactCOIServiceWorker';
 import StyledComponentsRegistry from './registry';
 
 export default function App({ Component, pageProps }: AppProps) {
