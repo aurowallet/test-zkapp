91d4af8640545ab2190877eba9e7e75c957bf71f
lvshaoping007 <1244849816@qq.com>
2024-05-26 21:10:32 +0800

    Add coi service worker

--- ui/src/pages/_app.page.tsx
+++ ui/src/pages/_app.page.tsx
@@ -3,6 +3,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import './reactCOIServiceWorker';
 import StyledComponentsRegistry from './registry';
 
 export default function App({ Component, pageProps }: AppProps) {
